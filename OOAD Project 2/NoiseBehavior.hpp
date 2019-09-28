0a6dfca6660bf34dc180314672df12dd256f2335
Kyle Schultz <kyle.j.schultz23@gmail.com>
2019-09-28 13:35:48 -0600

    Noise behavior

---
+++ OOAD Project 2/NoiseBehavior.hpp
@@ -0,0 +1,21 @@
+//
+//  NoiseBehavior.hpp
+//  OOAD Project 2
+//
+//  Created by Kyle Schultz on 9/28/19.
+//  Copyright © 2019 Kyle Schultz. All rights reserved.
+//
+
+#ifndef NoiseBehavior_hpp
+#define NoiseBehavior_hpp
+
+#include <stdio.h>
+#include <string>
+
+class NoiseBehavior {
+public:
+    //pure virtual function
+    virtual std::string makeNoise() = 0;
+};
+
+#endif /* NoiseBehavior_hpp */
